dfce95c069f50b289b1be2b312bbb8abc22787f0
PyaePhyoPaing-mfu <6531503175@lamduan.mfu.ac.th>
2025-06-29 14:00:49 +0700

    fix llm credentials

--- llm.py
+++ llm.py
@@ -4,16 +4,20 @@ import json
 import streamlit as st
 
 
+try:
+    raw = st.secrets["google"]["credentials"]
+    service_account_info = json.loads(raw)
+    credentials = Credentials.from_service_account_info(
+        service_account_info,
+        scopes=['https://www.googleapis.com/auth/cloud-platform']
+    )
 
-# key_path = 'botexpert-459607-26d1eb471087.json'
-
-raw = st.secrets["google"]["credentials"]
-service_account_info = json.loads(raw)
-credentials = Credentials.from_service_account_info(service_account_info, scopes=['https://www.googleapis.com/auth/cloud-platform'])
-
-llm = VertexAI(
-    project='machine-translation-001',
-    location='us-central1',
-    credentials=credentials,
-    model = "gemini-2.5-pro-preview-05-06"
-)
+    llm = VertexAI(
+        project='machine-translation-001',
+        location='us-central1',
+        credentials=credentials,
+        model = "gemini-2.5-pro-preview-05-06"
+    )
+except Exception as e:
+    st.error("LLM setup failed")
+    st.text(repr(e))
