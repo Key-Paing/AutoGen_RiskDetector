9c06f3678b3a5ab78ba599d1fb245810e8210287
PyaePhyoPaing-mfu <6531503175@lamduan.mfu.ac.th>
2025-07-02 11:57:49 +0700

    remove manual json file read

--- agents.py
+++ agents.py
@@ -1,45 +1,15 @@
 import autogen
 from autogen import UserProxyAgent
-from llm import llm
+from llm import config_list
 
-config_list = autogen.config_list_from_json("model_config_gemini.json")
+# config_list = autogen.config_list_from_json("model_config_gemini.json")
+# for config in config_list:
+#     config["credentials"] = llm.credentials
 
-# summary_agent = autogen.ConversableAgent(
-#     name="Summarizer",
-#     system_message="You’re a legal writer who converts complex legal text into concise summaries.",
-#     llm_config = {"config_list": config_list},
-#     code_execution_config = {"use_docker": False},
-#     human_input_mode="NEVER",
-# )
 
-# summary_agent = autogen.ConversableAgent(
-#     name="Summarizer",
-#     system_message="""You are a legal contract analysis expert and you will be followed by Language detecting agent. Your job is to:
-#             1. Analyze legal contracts in Burmese or English
-#             2. Extract key information: parties, terms, obligations, dates, amounts
-#             3. Identify important clauses and conditions
-#             4. Provide clear, structured summaries
-            
-#             For each contract, provide:
-#             - Document Type
-#             - Parties Involved
-#             - Key Terms and Conditions
-#             - Important Dates
-#             - Financial Information (if any)
-#             - Key Obligations
-#             - Risk Factors or Notable Clauses
-            
-#             Summarize in Burmese if the document is in Burmese, otherwise summarize in English.""",
-#     llm_config = {"config_list": config_list},
-#     code_execution_config = {"use_docker": False},
-#     human_input_mode="NEVER",
-# )
 
 risk_detecting_agent = autogen.ConversableAgent(
     name="Risk Detector",
-    # system_message="You are an expert in contract analysis with a focus on identifying risks and issues. "
-    #     "Your task is to thoroughly review the provided contract PDF files and highlight any potential risks, ambiguities, or problematic clauses. "
-    #     "You should consider legal implications, compliance issues, and any other factors that could pose risks to the parties involved in the contract.",
     system_message = """
 You are an expert in contract analysis, and your task is to identify potential **risks** in contract clauses.
 
@@ -97,13 +67,6 @@ Return the results in this format:
 
 )
 
-# contract_type_classifier_agent = autogen.ConversableAgent(
-#     name="Contract Type Classifier",
-#     system_message="You are an expert in contract analysis with a focus on identifying the type of contract. ",
-#     llm_config = {"config_list": config_list},
-#     code_execution_config = {"use_docker": False},
-#     human_input_mode="NEVER",
-#       )
 
 language_detector_agent = autogen.ConversableAgent(
     name="Language Detector",
@@ -122,3 +85,45 @@ language_detector_agent = autogen.ConversableAgent(
 )
 
 user = UserProxyAgent(name="User", human_input_mode="NEVER")
+
+
+
+# summary_agent = autogen.ConversableAgent(
+#     name="Summarizer",
+#     system_message="You’re a legal writer who converts complex legal text into concise summaries.",
+#     llm_config = {"config_list": config_list},
+#     code_execution_config = {"use_docker": False},
+#     human_input_mode="NEVER",
+# )
+
+# summary_agent = autogen.ConversableAgent(
+#     name="Summarizer",
+#     system_message="""You are a legal contract analysis expert and you will be followed by Language detecting agent. Your job is to:
+#             1. Analyze legal contracts in Burmese or English
+#             2. Extract key information: parties, terms, obligations, dates, amounts
+#             3. Identify important clauses and conditions
+#             4. Provide clear, structured summaries
+            
+#             For each contract, provide:
+#             - Document Type
+#             - Parties Involved
+#             - Key Terms and Conditions
+#             - Important Dates
+#             - Financial Information (if any)
+#             - Key Obligations
+#             - Risk Factors or Notable Clauses
+            
+#             Summarize in Burmese if the document is in Burmese, otherwise summarize in English.""",
+#     llm_config = {"config_list": config_list},
+#     code_execution_config = {"use_docker": False},
+#     human_input_mode="NEVER",
+# )
+
+
+# contract_type_classifier_agent = autogen.ConversableAgent(
+#     name="Contract Type Classifier",
+#     system_message="You are an expert in contract analysis with a focus on identifying the type of contract. ",
+#     llm_config = {"config_list": config_list},
+#     code_execution_config = {"use_docker": False},
+#     human_input_mode="NEVER",
+#       )
